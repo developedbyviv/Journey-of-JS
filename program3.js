2d974f4300215e34445a515dce2632b614d1a77b
Vivek Singh Bhandari <developedbyviv@gmail.com>
2022-10-07 05:28:44 +0530

    commit program3

---
+++ program3.js
@@ -0,0 +1,12 @@
+// Write a JavaScript program to create a new string from a given string taking the last 3 characters and added at both the front and back.
+// The string length must be 3 or more
+
+let swapChars = (str) => {
+  if (str.length >= 3) {
+    backChars = str.substring(str.length - 3); // stores last three characters
+    return backChars + str + backChars;
+  } else {
+    return false;
+  }
+};
+console.log(swapChars("Swift"));
